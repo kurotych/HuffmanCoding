cc2abf9fb5d070a2990357877d3a39dff06c6f43
kurotych <akurotych@gmail.com>
2017-10-29 18:17:40 +0200

    add 14 standart and counting symbols

--- main.cpp
+++ main.cpp
@@ -2,26 +2,41 @@
 #include <queue>
 #include <vector>
 #include <memory>
+#include <algorithm>
 
 using Unit = std::pair<char, int>;
 using QueueContainer = std::vector<Unit>;
 
 template<typename T>
 using queue_t = std::priority_queue<Unit, QueueContainer, T>;
+
 template<typename T>
 using ptr_queue_t = std::unique_ptr<queue_t<T>>;
 
 decltype(auto)
 create_queue(const std::string& s)
 {
+    std::vector<Unit> v;
+    v.reserve(10);
+
+    for(auto c : s)
+    {
+        if ( std::find_if(v.begin(), v.end(),
+                          [c](Unit& u) {
+                               if (u.first == c) {
+                                    u.second++;
+                                    return true;
+                               }
+                               return false;
+                          } ) == v.end() )
+            v.push_back(Unit(c,1));
+    }
+
     auto comp = [] (const Unit &a, const Unit &b)
     { return a.second > b.second; };
 
-    auto ptr = ptr_queue_t<decltype(comp)>(new queue_t<decltype(comp)>(comp));
-
-    ptr->push(Unit('a',4));
-    ptr->push(Unit('b',2));
-    ptr->push(Unit('c',3));
+    auto ptr = ptr_queue_t<decltype(comp)>
+            (new queue_t<decltype(comp)>(comp, std::move(v)));
 
     return ptr;
 }
