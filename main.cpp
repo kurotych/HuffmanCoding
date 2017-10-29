456a015b14723aadf6dfe938bdffce4be7019c88
kurotych <akurotych@gmail.com>
2017-10-29 22:39:24 +0200

    add leaf and node

--- main.cpp
+++ main.cpp
@@ -4,52 +4,86 @@
 #include <memory>
 #include <algorithm>
 
-using Unit = std::pair<char, int>;
-using QueueContainer = std::vector<Unit>;
+class Element
+{
+public:
+    virtual bool isLeaf() = 0;
+    virtual ~Element(){}
+};
 
-template<typename T>
-using queue_t = std::priority_queue<Unit, QueueContainer, T>;
+class Leaf : public Element
+{
+public:
+    Leaf(char _symbol) : symbol(_symbol),
+        frequency(1){ }
+    char symbol;
+    int frequency;
+    bool isLeaf() override { return true; }
+};
 
-template<typename T>
-using ptr_queue_t = std::unique_ptr<queue_t<T>>;
+class Node : public Element
+{
+public:
+    Node():frequency(0), left(nullptr),
+        right(nullptr){}
+    int frequency;
+    Element * left;
+    Element * right;
+    bool isLeaf() override { return false; }
+};
 
 decltype(auto)
 create_queue(const std::string& s)
 {
-    std::vector<Unit> v;
+    std::vector<Element*> v;
     v.reserve(10);
 
     for(auto c : s)
     {
         if ( std::find_if(v.begin(), v.end(),
-                          [c](Unit& u) {
-                               if (u.first == c) {
-                                    u.second++;
+                          [c](Element* u) {
+                               Leaf * l = dynamic_cast<Leaf*>(u);
+                               if (l->symbol == c) {
+                                    l->frequency++;
                                     return true;
                                }
                                return false;
                           } ) == v.end() )
-            v.push_back(Unit(c,1));
+        {
+            v.push_back(new Leaf(c));
+        }
     }
 
-    auto comp = [] (const Unit &a, const Unit &b)
-    { return a.second > b.second; };
+    auto comp = [] (const Element *a, const Element *b)
+    { return ((const Leaf *)a)->frequency > ((const Leaf *)b)->frequency; };
 
-    auto ptr = ptr_queue_t<decltype(comp)>
-            (new queue_t<decltype(comp)>(comp, std::move(v)));
+    using queue_t = std::priority_queue<Element*, std::vector<Element*>, decltype(comp)>;
 
+    auto delEl = [](queue_t * q)
+    {
+        while(!q->empty())
+        {
+            delete q->top();
+            q->pop();
+        }
+        delete q;
+    };
+
+    std::unique_ptr<queue_t, decltype(delEl)> ptr(new queue_t(comp, v), delEl);
     return ptr;
 }
+
+
 
 int main()
 {
     auto q = create_queue("abacabad");
 
-    while(!q->empty())
-    {
-        std::cout << q->top().first << " " << q->top().second << std::endl;
-        q->pop();
-    }
+//    while(!q->empty())
+//    {
+//        std::cout << ((Leaf*)q->top())->symbol << " " << ((Leaf*)q->top())->frequency << std::endl;
+//        q->pop();
+//    }
 
     return 0;
 }
